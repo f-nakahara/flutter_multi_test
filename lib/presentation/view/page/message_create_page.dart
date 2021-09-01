a70f23c8819b65271b5357236e3ae8c75cd3285c
Nakahara <50205590+f-nakahara@users.noreply.github.com>
2021-09-01 19:00:33 +0900

    message create page

---
+++ lib/presentation/view/page/message_create_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MessageCreatePage extends StatelessWidget {
+  const MessageCreatePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('メッセージ作成画面'),
+      ),
+    );
+  }
+}
